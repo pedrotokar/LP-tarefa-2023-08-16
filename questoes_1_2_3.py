5875504d7b6a1c3f2c8cbab34dfda199a1b6a9d1
KaikyBraga <kaiky.braga06221607@gmail.com>
2023-08-16 09:06:40 -0300

    Resultado das funções

--- questoes_1_2_3.py
+++ questoes_1_2_3.py
@@ -6,6 +6,7 @@ def questao_1():
     array_2 = np.arange(11,20)
     # Soma dos elementos dos arrays elemento a elemento:
     array_3 = array_1 + array_2
+    print(array_3)
     return array_3
 
 def questao_2():
@@ -15,6 +16,7 @@ def questao_2():
     array_redimensionado = array_redimensionado.astype(float)
     # Matriz transposta:
     array_redimensionado = array_redimensionado.T
+    print(array_redimensionado)
     return array_redimensionado
 
 def questao_3():
@@ -24,4 +26,5 @@ def questao_3():
     array_4 = array_4.reshape(2,3)
     # Multiplicação entre matrizes:
     array_4 = np.dot(array_4, questao_2())
+    print(array_4)
     return array_4
