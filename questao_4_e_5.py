5e3cfd7e8bb09bc8eccf956ab2bfb402478a0d7d
pedrotokar <pedrotokar2004@gmail.com>
2023-08-16 09:06:23 -0300

    Terminei as questoes 4 e 5

--- questao_4_e_5.py
+++ questao_4_e_5.py
@@ -6,33 +6,36 @@ def questao_4():
     # inteiros entre os valores fornecidos
     array_aleatoria_1 = npr.randint(0, 10, 8)
     array_aleatoria_2 = npr.randint(0, 10, 8)
+    print(f"Quinta array: {array_aleatoria_1}")
+    print(f"Sexta array: {array_aleatoria_1}")
 
     # A função np.intersect1d retorna uma array com a interesecção entre duas
     # arrays unidimensionais. O argumento return_indices, quando verdadeiro, faz
     # a função retornar mais duas arrays extras contendo os índices de cada item
     # nas duas arrays inciais.
-    interseccao = np.intersect1d(
+    interseccao, indices_1, indices_2 = np.intersect1d(
         array_aleatoria_1,
         array_aleatoria_2,
         return_indices = True
     )
-
-    return array_aleatoria_1, array_aleatoria_2, interseccao
+    print(f"Elementos comuns das duas arrays: {interseccao}")
+    print("Índices correspondentes dos elementos comuns:")
+    for index in range(indices_1.shape[0]):
+        print(f"{indices_1[index]} - {indices_2[index]}")
+    print("\n")
+    return array_aleatoria_1, array_aleatoria_2
 
 def questao_5():
-    array_5, array_6, interseccao = questao_4()
+    array_5, array_6 = questao_4()
 
     # A função np.hstack concatena as duas arrays horizontalmente
     array_7 = np.hstack((array_5, array_6))
-    print(array_7)
+    print(f"Sétima array: {array_7}")
 
     # As funções np.mean, np.std e np.var retornam respectivamente a média,
     # desvio padrão e variância de uma array unidimensional
-    estatisticas = {
-        "media": np.mean(array_7),
-        "desvio-padrao":  np.std(array_7),
-        "variancia": np.var(array_7)
-    }
+    print(f"Medidas estatísticas da sétima array:\nMédia: {np.mean(array_7)}\n"\
+          f"Desvio Padrão:  {np.std(array_7)}\nVariância: {np.var(array_7)}")
 
     # Define uma função que recebe um inteiro e retorna -1 ou 1 de acordo com
     # a paridade dele
@@ -42,10 +45,10 @@ def questao_5():
         else:
             return 1
 
+    # A função vectorize faz uma funcao aceitar um vetor e faz ela retornar outro
+    # vetor com os elementos modificados pela fucao original
     ver_paridade_vetorizada = np.vectorize(ver_paridade)
-    return ver_paridade_vetorizada(array_7)
+    vetor_7 = ver_paridade_vetorizada(array_7)
+    print(f"Sétima array modificada: {vetor_7}", end = "\n\n")
 
-
-
-print(questao_4())
-print(questao_5())
+questao_5()
