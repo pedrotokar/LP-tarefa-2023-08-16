5d4c39295b7a72fcee815278cfe209c472089ed1
pedrotokar <pedrotokar2004@gmail.com>
2023-08-16 08:32:22 -0300

    Fiz metade da questao 5

--- questao_4_e_5.py
+++ questao_4_e_5.py
@@ -2,7 +2,26 @@ import numpy as np
 import numpy.random as npr
 
 def questao_4():
-    pass
+    # A função `npr.randint` retorna uma array de n elementos que são números
+    # inteiros entre os valores fornecidos
+    array_aleatoria_1 = npr.randint(0, 10, 8)
+    array_aleatoria_2 = npr.randint(0, 10, 8)
+
+    # A função `np.intersect1d` retorna uma array com a interesecção entre duas
+    # arrays unidimensionais. O argumento return_indices, quando verdadeiro, faz
+    # a função retornar mais duas arrays extras contendo os índices de cada item
+    # nas duas arrays inciais.
+    interseccao = np.intersect1d(
+        array_aleatoria_1,
+        array_aleatoria_2,
+        return_indices = True
+    )
+    print(interseccao)
+
+    return array_aleatoria_1, array_aleatoria_2, interseccao
 
 def questao_5():
     pass
+
+print(questao_4())
+print(questao_5())
