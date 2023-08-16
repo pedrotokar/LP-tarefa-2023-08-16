a85c4c580c5878bee374282b8698d07a304a2e17
pedrotokar <pedrotokar2004@gmail.com>
2023-08-16 08:52:51 -0300

    Fiz a base da quinta questao e adicionei elementos à quarta

--- questao_4_e_5.py
+++ questao_4_e_5.py
@@ -7,7 +7,7 @@ def questao_4():
     array_aleatoria_1 = npr.randint(0, 10, 8)
     array_aleatoria_2 = npr.randint(0, 10, 8)
 
-    # A função `np.intersect1d` retorna uma array com a interesecção entre duas
+    # A função np.intersect1d retorna uma array com a interesecção entre duas
     # arrays unidimensionais. O argumento return_indices, quando verdadeiro, faz
     # a função retornar mais duas arrays extras contendo os índices de cada item
     # nas duas arrays inciais.
@@ -16,12 +16,36 @@ def questao_4():
         array_aleatoria_2,
         return_indices = True
     )
-    print(interseccao)
 
     return array_aleatoria_1, array_aleatoria_2, interseccao
 
 def questao_5():
-    pass
+    array_5, array_6, interseccao = questao_4()
+
+    # A função np.hstack concatena as duas arrays horizontalmente
+    array_7 = np.hstack((array_5, array_6))
+    print(array_7)
+
+    # As funções np.mean, np.std e np.var retornam respectivamente a média,
+    # desvio padrão e variância de uma array unidimensional
+    estatisticas = {
+        "media": np.mean(array_7),
+        "desvio-padrao":  np.std(array_7),
+        "variancia": np.var(array_7)
+    }
+
+    # Define uma função que recebe um inteiro e retorna -1 ou 1 de acordo com
+    # a paridade dele
+    def ver_paridade(numero):
+        if numero % 2:
+            return -1
+        else:
+            return 1
+
+    ver_paridade_vetorizada = np.vectorize(ver_paridade)
+    return ver_paridade_vetorizada(array_7)
+
+
 
 print(questao_4())
 print(questao_5())
